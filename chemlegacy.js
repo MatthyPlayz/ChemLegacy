b30cbbda755c4e38f6eab57d77fb2cb77db00edd
matthyno <34276633+MatthyPlayz@users.noreply.github.com>
2019-11-17 10:21:21 -0500

    Update chemlegacy.js

--- chemlegacy.js
+++ chemlegacy.js
@@ -15,7 +15,7 @@ func:function()
 		turnToByContext:{'eat':{'health':-1,'happiness':0.03},'decay':{'spoiled food':0.5}},
 		partOf:'advanced building materials'
 	});
-	G.getRes(saltwater.name).gainedBy[1] = "Gatherer"
+	G.getRes("saltwater").gainedBy[1] = "Gatherer"
 	new G.Tech({
 		name:'gathering saltwater',
 		desc:'@[gatherer]s can be gathered from lakes or ponds.\nAlso starts the Hydrochloric Acid quest chain in the ChemLegacy mod.',
